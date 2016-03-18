947a9922800517243cf44be023b90790c5e20ac0
rodrigopavezi <rodrigopavezi@gmail.com>
2016-03-18 14:09:12 -0300

    Implemented the use of the feed parser

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,14 @@
   "use strict";
 
   var gulp = require("gulp");
-  var browserify = require('browserify');
   var bump = require("gulp-bump");
   var jshint = require("gulp-jshint");
-  var rename = require("gulp-rename");
-  var uglify = require('gulp-uglify');
   var colors = require("colors");
   var transform = require('vinyl-transform');
   var runSequence = require("run-sequence");
   var wct = require("web-component-tester").gulp.init(gulp);
   var bower = require("gulp-bower");
   var del = require("del");
-
-  gulp.task("browserify", function () {
-    var browserified = transform(function(filename) {
-      var b = browserify(filename);
-      return b.bundle();
-    });
-
-    return gulp.src(["browserify.js"])
-      .pipe(browserified)
-      .pipe(uglify())
-      .pipe(rename("modules.js"))
-      .pipe(gulp.dest(""));
-  });
 
   gulp.task("clean-bower", function(cb){
     del(["./bower_components/**"], cb);
@@ -58,7 +42,7 @@
   });
 
   gulp.task("build", function (cb) {
-    runSequence("lint", "browserify", cb);
+    runSequence("lint", cb);
   });
 
   gulp.task("default", [], function() {
